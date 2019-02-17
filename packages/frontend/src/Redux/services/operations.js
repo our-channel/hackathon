6405987e8a7cda30ad0dcc3891ffeb4fe2082983
mdcoon <mdcoon1@yahoo.com>
2019-02-17 05:23:13 -0700

    Took out ID con service until after signup

--- packages/frontend/src/Redux/services/operations.js
+++ packages/frontend/src/Redux/services/operations.js
@@ -14,22 +14,46 @@ const init = () => async (dispatch,getState) => {
   if(account) {
     account = account.toLowerCase();
   }
-
-  let contractAddress = state.signup.localAccounts[0].contractAddress;
   let idFactory = new IDFactoryService({web3});
-  let idContract = new IDContractService({
-    web3,
-    account,
-    contractAddress
-  });
   let userRegistry = new UserRegistryService({
     web3,
     account,
     contractAddress: "0xbea9c7ab291a4252c9f259ddeb57657469cc6e84"
   });
+
+  if(state.signup.localAccounts.length === 0) {
+    dispatch(Creators.initSuccess(null, idFactory, userregistry))
+    return;
+  }
+
+  let contractAddress = state.signup.localAccounts[0].contractAddress;
+
+  let idContract = new IDContractService({
+    web3,
+    account,
+    contractAddress
+  });
+
+  dispatch(Creators.initSuccess(idContract, idFactory, userRegistry));
+}
+
+const initIdContract = addr => (dispatch,getState) => {
+  let state = getState();
+  let w3 = state.web3;
+  let web3 = w3.web3;
+  let account = w3.account.toLowerCase();
+  let idFactory = state.services.idFactory;
+  let userRegistry = state.services.userRegistry;
+  let idContract = new IDContractService({
+    web3,
+    account,
+    contractAddress: addr
+  });
+
   dispatch(Creators.initSuccess(idContract, idFactory, userRegistry));
 }
 
 export default {
-  init
+  init,
+  initIdContract
 }
